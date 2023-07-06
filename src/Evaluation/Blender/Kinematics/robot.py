a7b25adb82f12c12c53e3ecf48ac9be4cecdaf99
Roman Parak <Roman.Parak@outlook.com>
2023-07-06 10:21:54 +0200

    improvements of the repo.

--- src/Evaluation/Blender/Kinematics/robot.py
+++ src/Evaluation/Blender/Kinematics/robot.py
@@ -22,7 +22,7 @@ Description:
     Initialization of constants.
 """
 # Set the structure of the main parameters of the controlled robot.
-CONST_ROBOT_TYPE = Parameters.ABB_IRB_14000_L_Str
+CONST_ROBOT_TYPE = Parameters.ABB_IRB_120_Str
 # Set the structure of the main parameters of the camera.
 CONST_CAMERA_TYPE = Lib.Blender.Parameters.Camera.Right_View_Camera_Parameters_Str
 
