dc02b3e15f66290b28ee89ca8c27a659bb05efad
Roman Parak <Roman.Parak@outlook.com>
2023-07-23 10:29:25 +0200

    Update t1.py

--- src/Evaluation/Blender/Collider/t1.py
+++ src/Evaluation/Blender/Collider/t1.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-n_joints = np.array([1,2,3,4,5,6])
+n_joints = np.array([1,2,3,4])
 
-
+offset = 0
 for i, n_i in enumerate(n_joints):
-    for _, n_ij in enumerate(n_joints[(i + 2)::]):
+    for _, n_ij in enumerate(n_joints[(i + offset)::]):
         print(n_i, n_ij)
 
