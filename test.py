048fb8352fa5a06f42d9de22468752a44a568642
Roman Parak <Roman.Parak@outlook.com>
2023-08-02 14:21:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -16,10 +16,10 @@ p.setGravity(0,0,-9.81)
 p.setTimeStep(1.0/100.0)
 p.setRealTimeSimulation(0)
 
-base_id = p.loadURDF('URDFs/Robots/ABB_IRB_14000_Base/ABB_IRB_14000_Base.urdf', [0.0, 0.0, 0.0], [0, 0, 0, 1], useFixedBase=True)
+#base_id = p.loadURDF('URDFs/Robots/ABB_IRB_14000_Base/ABB_IRB_14000_Base.urdf', [0.0, 0.0, 0.0], [0, 0, 0, 1], useFixedBase=True)
 
 #this will disable all collisions with the cubeId
-p.setCollisionFilterGroupMask(base_id, -1, 0, 0)
+#p.setCollisionFilterGroupMask(base_id, -1, 0, 0)
 
 #this would enable/disable collision between a specific pair of objects
 #enableCollision = 1
@@ -32,11 +32,12 @@ P2 = Robot_Param.ABB_IRB_14000_R_Str.T.Base.p
 Q2 = Robot_Param.ABB_IRB_14000_R_Str.T.Base.Get_Rotation('QUATERNION')
 
 #quaternion: x, y, z, w
-obj_id_1 = p.loadURDF('URDFs/Robots/ABB_IRB_14000_L/ABB_IRB_14000_L.urdf', [P1.x, P1.y, P1.z], [Q1.x, Q1.y, Q1.z, Q1.w], useFixedBase=True, flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES)
+#obj_id_1 = p.loadURDF('URDFs/Robots/ABB_IRB_14000_L/ABB_IRB_14000_L.urdf', [P1.x, P1.y, P1.z], [Q1.x, Q1.y, Q1.z, Q1.w], useFixedBase=True, flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES)
 #obj_id_2 = p.loadURDF('URDFs/Robots/ABB_IRB_14000_R/ABB_IRB_14000_R.urdf', [P2.x, P2.y, P2.z], [Q2.x, Q2.y, Q2.z, Q2.w], useFixedBase=True, flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES)
 
 
-p.setCollisionFilterPair(obj_id_1, base_id, -1,-1, 1)
+#p.setCollisionFilterPair(obj_id_1, base_id, -1,-1, 1)
+#p.addUserDebugLine([0.5, 0.0, 0.0], [0.5, 0.0, 1.0], [0.0, 1.0, 0.2], 1.0)
 
 while p.isConnected():
     p.stepSimulation()
