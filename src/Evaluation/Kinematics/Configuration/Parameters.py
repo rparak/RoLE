bcb6450c4e0b3a0adb7eaf47bb370c271923054d
Roman Parak <Roman.Parak@outlook.com>
2023-10-06 13:46:38 +0200

    Update Parameters.py

--- src/Evaluation/Kinematics/Configuration/Parameters.py
+++ src/Evaluation/Kinematics/Configuration/Parameters.py
@@ -31,6 +31,11 @@ def Get_Absolute_Joint_Positions(name: str) -> tp.Tuple[tp.List[float],
     """
 
     return {
+        'Universal_Robots_UR3': (None, None),
+        'ABB_IRB_120': (None, None),
+        'ABB_IRB_120_L_Ax': (None, None),
+        'ABB_IRB_14000_R': (None, None),
+        'ABB_IRB_14000_L': (None, None),
         'EPSON_LS3_B401S': (np.array([Mathematics.Degree_To_Radian(0.0), Mathematics.Degree_To_Radian(0.0), 0.0, Mathematics.Degree_To_Radian(0.0)], 
                                      dtype = np.float64), 
                             np.array([Mathematics.Degree_To_Radian(115.0), Mathematics.Degree_To_Radian(-20.0), 0.10, Mathematics.Degree_To_Radian(15.0)],
