f2aa60469d7d32f8754314a034cd803af2a2506a
Roman Parak <59449320+rparak@users.noreply.github.com>
2023-10-13 10:05:25 +0200

    Update t1.py

--- src/Evaluation/Kinematics/IK/t1.py
+++ src/Evaluation/Kinematics/IK/t1.py
@@ -12,12 +12,14 @@ import time
 import Lib.Parameters.Robot as Parameters
 #   ../Lib/Kinematics/Core
 import Lib.Kinematics.Core
+#   ../Lib/Transformation/Core
+import Lib.Transformation.Core as Transformation
 #   ../Lib/Transformation/Utilities/Mathematics
 import Lib.Transformation.Utilities.Mathematics as Mathematics
-#   ../Lib/Trajectory/Utilities
-import Lib.Trajectory.Utilities
 #   ../Configuration/Parameters
 import Configuration.Parameters
+#   ..
+import Lib.Interpolation.Utilities
 
 """
 Description:
@@ -38,18 +40,24 @@ def main():
     # Obtain the constraints for absolute joint positions in order to generate multi-axis position trajectories.
     (abs_j_pos_0, abs_j_pos_1) = Configuration.Parameters.Get_Absolute_Joint_Positions(Robot_Str.Name)
 
-    # ...
+    # Get the actual and desired tool center point (TCP).
+    T_actual  = Lib.Kinematics.Core.Forward_Kinematics(abs_j_pos_0, 'Fast', Robot_Str)[1]
     T_desired = Lib.Kinematics.Core.Forward_Kinematics(abs_j_pos_1, 'Fast', Robot_Str)[1]
 
-    # Obtain the spherical linear interpolation (Slerp) between the given quaternions.
-    #q = Utilities.Slerp('Quaternion', q_0, q_1, t_i)
-    
-    print('[INFO] The calculation is in progress.')
-    t_0 = time.time()
+    # ...
+    t = np.linspace(Configuration.Parameters.CONST_T_0, Configuration.Parameters.CONST_T_1, 100)
 
-    # Calculation of inverse kinematics (IK) using the analytical method.
-    theta_0 = abs_j_pos_0.copy(); theta_T = np.array(theta_arr, dtype=np.float64).T
-    for _, theta_arr_i in enumerate(theta_T):
+    theta_0 = abs_j_pos_0.copy(); 
+    for _, t_i in enumerate(t):
+        # ...
+        p_i = Lib.Interpolation.Utilities.Lerp('Explicit', T_actual.p.all(), T_desired.p.all(), t_i)
+
+        # Obtain the spherical linear interpolation (Slerp) between the given quaternions.
+        q_i = Lib.Interpolation.Utilities.Slerp('Quaternion', T_actual.Get_Rotation('QUATERNION'), 
+                                              T_desired.Get_Rotation('QUATERNION'), t_i)
+        
+        # Express the homogeneous transformation matrix of an object from position and rotation.
+        TCP_Position = Transformation.Homogeneous_Transformation_Matrix_Cls(None, np.float64).Rotation(q_i.all(), 'QUATERNION').Translation(p_i)
 
         # Obtain the absolute positions of the joints from the input homogeneous transformation matrix of the robot's end-effector.
         #   IK:
@@ -59,16 +67,12 @@ def main():
         # Obtain the last absolute position of the joint.
         theta_0 = theta.copy()
 
-    t = time.time() - t_0
-    print(f'[INFO] Time: {t:0.05f} in seconds.')
-
-    # Get the actual and desired tool center point (TCP) to check the results.
-    T_desired = Lib.Kinematics.Core.Forward_Kinematics(abs_j_pos_1, 'Fast', Robot_Str)[1]
-    T_actual  = Lib.Kinematics.Core.Forward_Kinematics(theta, 'Fast', Robot_Str)[1]
+    # Get the observed tool center point (TCP) to check the results.
+    T_observed = Lib.Kinematics.Core.Forward_Kinematics(theta, 'Fast', Robot_Str)[1]
 
     # Check that the calculation has been performed successfully.
-    accuracy = Mathematics.Euclidean_Norm((T_actual - T_desired).all())
-    if Mathematics.Euclidean_Norm((T_actual - T_desired).all()) <= 1e-5:
+    accuracy = Mathematics.Euclidean_Norm((T_observed - T_desired).all())
+    if Mathematics.Euclidean_Norm((T_observed - T_desired).all()) <= 1e-5:
         print('[INFO] The IK solution test was successful.')
         print(f'[INFO] Accuracy = {accuracy}')
     else:
