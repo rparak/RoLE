938df184ac973a533af68901b4b1ced091b8b0d8
Roman Parak <59449320+rparak@users.noreply.github.com>
2023-11-07 11:48:56 +0100

    Update Mechanism.py

--- src/Lib/Parameters/Mechanism.py
+++ src/Lib/Parameters/Mechanism.py
@@ -17,9 +17,6 @@ class Theta_Parameters_Str(object):
     """
     Description:
         The auxiliary structure of the joint (theta) parameters.
-
-        Note:
-            Private structure.
     """
 
     # Zero absolute position of each joint.
@@ -53,9 +50,6 @@ class T_Parameters_Str:
     """
     Description:
         The auxiliary structure of the homogeneous transformation matrix {T} parameters.
-
-        Note:
-            Private structure.
     """
 
     # Homogeneous transformation matrix of the base.
